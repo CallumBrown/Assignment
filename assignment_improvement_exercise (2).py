0a1570f3e322c575a43ae43eb122875e89f773cf
44075 <44075@longroad.ac.uk>
2014-09-12 13:45:43 +0100

    Assignment improvemnet exercise

---
+++ assignment_improvement_exercise (2).py
@@ -0,0 +1,16 @@
+#Callum Brown
+#12-09-14
+#Variable improvement exercise
+
+import math
+
+radius = int(input("please enter the radius of the circle: "))
+
+circumference = 2* math.pi * radius
+circumference = round(circumference,2)
+
+area = math.pi * radius**2
+area = round(area,2)
+
+print("The circumference of this circle is {0}.".format(circumference))
+print("The area of this circle is {0}.".format(area))
