2b05e469e701cd5195951ab33b16f6ed016fdea3
44075 <44075@longroad.ac.uk>
2014-09-12 14:16:53 +0100

    development exercise 1, div and mod

---
+++ development exercise 1.py
@@ -0,0 +1,16 @@
+#Callum Brown
+#12-09-14
+#Development exercise 1
+
+
+
+number1 = int(input("Please enter your first number: "))
+number2 = int(input("Please enter your last number: "))
+
+div = number1/number2
+mod = number1%number2
+
+print("The whole number answer is {0}, and the remainder is {1} !".format( div, mod))
+
+
+
