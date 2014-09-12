789b5c03ee5dbfcb02056e10bcdea320c12fa5ee
44075 <44075@longroad.ac.uk>
2014-09-12 14:34:15 +0100

    fahrenhiet to centrigrade temperature

---
+++ Fahrenheit to centigrade exercise.py
@@ -0,0 +1,10 @@
+#Callum Brown
+#12-09-14
+#Development exercise 2
+
+fahrenheit = float(input("Please enter the temperature in Fahrenheit: "))
+
+centigrade = (fahrenheit-32)*(5/9)
+
+print("The temperature in degrees Centigrade is {0}.".format(centigrade, fahrenheit))
+
