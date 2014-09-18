9b1bfb9ada627f740f431d24013f4545d355ebb5
44075 <44075@longroad.ac.uk>
2014-09-18 10:11:02 +0100

    denary to hexadecimal

---
+++ deanary to hexadecimal - task 1.py
@@ -0,0 +1,12 @@
+#Callum Brown
+#18-90-14
+#Denary to hexadecimal - Task 1
+
+denary_value=int(input("Please enter a denary number: "))
+
+binary_string = ""
+while denary_value > 0:
+      binary_string = str(denary_value %2) + binary_string
+      denary_value = denary_value // 2
+
+print("The binary equivalent is: {0}".format(binary_string))
