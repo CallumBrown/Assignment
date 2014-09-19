a85643831a49ef7441fee3883ba0bed420e1ad82
44075 <44075@longroad.ac.uk>
2014-09-19 14:31:13 +0100

    swimming pool

---
+++ exercise circular swimming pool- maths.py
@@ -0,0 +1,14 @@
+#Callum Brown
+#19-09-14
+#Exercise -circular swimming pool
+
+depth= float(input("Please enter the height of the pool: "))
+diametre = float(input("Please enter the diametre of the pool: "))
+
+volume =math.pi(diametre/2)*depth
+
+print("The volume is {0}".format(volume))
+
+
+
+
